1a13ddf9f9e10ee7a4150a9b8bc3eeda65662065
Aarif Khan <aarifkhan7896@gmail.com>
2022-09-12 20:31:38 +0530

    Connection String - SQL
    ASP.NET to SQL Server - Signup Form

--- Project/Signup.aspx.cs
+++ Project/Signup.aspx.cs
@@ -21,7 +21,32 @@ namespace Project
 
         protected void signUpBtn_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                SqlConnection sqlCon = new SqlConnection(dbconn);
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO members(first_name,last_name,email,contact,address,pincode,password,status) values(@fname,@lname,@email,@contact,@address,@pin,@password,@status)", sqlCon);
+
+                cmd.Parameters.AddWithValue("@fname", fname.Text.Trim());
+                cmd.Parameters.AddWithValue("@lname", lname.Text.Trim());
+                cmd.Parameters.AddWithValue("@email", email.Text.Trim());
+                cmd.Parameters.AddWithValue("@contact", contact.Text.Trim());
+                cmd.Parameters.AddWithValue("@address", address.Text.Trim());
+                cmd.Parameters.AddWithValue("@pin", pincode.Text.Trim());
+                cmd.Parameters.AddWithValue("@password", password.Text.Trim());
+                cmd.Parameters.AddWithValue("@status", "Pending");
+                cmd.ExecuteNonQuery();
+                sqlCon.Close();
+                Response.Write("<script></script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
         }
     }
 }
