538787fe5244eb4cfba85ebdb08d5912378ae1ba
Aarif Khan <aarifkhan7896@gmail.com>
2022-09-13 17:06:05 +0530

    Login Page C#
    Users can login now

--- Project/Login.aspx.cs
+++ Project/Login.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +12,7 @@ namespace Project
 {
     public partial class Login : System.Web.UI.Page
     {
+        string dbconn = ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +20,32 @@ namespace Project
 
         protected void LoginBtn_Click(object sender, EventArgs e)
         {
-            Response.Redirect("~/Dashboard.aspx");
+            try
+            {
+                SqlConnection sqlCon = new SqlConnection(dbconn);
+                if(sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                SqlCommand cmd = new SqlCommand("select * from members where email = '"+email.Text.Trim()+"' and password = '"+password.Text.Trim()+"'", sqlCon);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Response.Write("<script>alert('"+reader.GetValue(3)+"')</script>");
+                    }
+                    Response.Redirect("~/Dashboard.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Credentials')</script>");
+                }
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>Console.log('"+ ex.Message + "')</script>");
+            }
         }
     }
 }
