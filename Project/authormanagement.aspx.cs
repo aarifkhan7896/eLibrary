95954eb8c81d63b785d8e08fdfc11cfe1568d7eb
Aarif Khan <aarifkhan7896@gmail.com>
2022-09-15 17:05:36 +0530

    Author Management Page Added
    author management

---
+++ Project/authormanagement.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project
+{
+    public partial class authormanagement : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void search_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void insert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void delete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
