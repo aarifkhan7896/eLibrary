6ecd26a1e7a3f859cb80362bd0392f560a687466
Aarif Khan <aarifkhan7896@gmail.com>
2022-09-15 17:41:16 +0530

    Add New Author C#
    we can now add a new author if it does not exists

--- Project/authormanagement.aspx.cs
+++ Project/authormanagement.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +13,7 @@ namespace Project
 {
     public partial class authormanagement : System.Web.UI.Page
     {
+        string dbconn = ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,12 +21,31 @@ namespace Project
 
         protected void search_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                SqlConnection sqlCon = new SqlConnection(dbconn);
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                SqlCommand cmd = new SqlCommand("", sqlCon);
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>console.log('" + ex.Message + "')</script>");
+            }
         }
 
         protected void insert_Click(object sender, EventArgs e)
         {
-
+            if (checkIfAuthorExists())
+            {
+                Response.Write("<script>alert('Author already exists')</script>");
+            }
+            else
+            {
+                addnewAuthor();
+            }
         }
 
         protected void update_Click(object sender, EventArgs e)
@@ -32,6 +56,64 @@ namespace Project
         protected void delete_Click(object sender, EventArgs e)
         {
 
+        }
+
+        bool checkIfAuthorExists()
+        {
+            try
+            {
+                SqlConnection sqlCon = new SqlConnection(dbconn);
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * from author where author_name = '" + authorname.Text.Trim() + "'", sqlCon);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                if (dataTable.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>console.log('" + ex.Message + "')</script>");
+                return false;
+            }
+        }
+        void addnewAuthor()
+        {
+            try
+            {
+                SqlConnection sqlCon = new SqlConnection(dbconn);
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                if (authorname.Text == null || authorname.Text == "")
+                {
+                    Response.Write("<script>alert('Author Name is Required')</script>");
+                }
+                else
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO author(author_name) values(@author_name)", sqlCon);
+                    cmd.Parameters.AddWithValue("@author_name", authorname.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    sqlCon.Close();
+                    Response.Write("<script>alert('Author Added')</script>");
+                    authorname.Text = "";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>console.log('" + ex.Message + "')</script>");
+            }
         }
     }
 }
